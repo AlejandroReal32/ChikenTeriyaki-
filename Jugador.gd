3f6a588bc4ab51044ba7b657cae743ba679b9cfa
Alejandro Real Lopez <100401041+AlejandroReal32@users.noreply.github.com>
2022-09-21 14:43:06 -0500

    juagador actualizado
    Jugador actualizado ahora ya puede brincar

--- Jugador.gd
+++ Jugador.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-var velocidad:int = 200
-var brinco:int = 600
+var velocidad:int = 400
+var brinco:int = 500
 var gravedad:int = 800
 var vel:Vector2 = Vector2()
 var piso:bool = false
@@ -15,4 +15,6 @@ func _physics_process(delta):
 		vel.x += velocidad
 	vel = move_and_slide(vel,Vector2.UP)
 	vel.y += gravedad * delta
+	if Input.is_action_pressed("ui_up") and is_on_floor() :
+		vel.y -= brinco
 	pass
