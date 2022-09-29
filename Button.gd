fdb7ec154d20875457483eba1c23f42406cca3ef
Alejandro Real Lopez <100401041+AlejandroReal32@users.noreply.github.com>
2022-09-29 09:02:44 -0600

    Escena Morir
    El jugador muere cuando cae y  tiene la opcion de reiniciar o salir del juego

---
+++ Button.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_Reiniciar_pressed():
+	get_tree().change_scene("res://escena.tscn")
---
+++ SALIDA.gd
@@ -0,0 +1,11 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _on_Salir_pressed():
+	get_tree().quit()
